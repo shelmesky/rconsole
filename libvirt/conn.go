bdb04cc0cbbb6b5326295f08fd106d5e82fb855d
roy.lieu <roy.lieu@gmail.com>
2015-04-02 17:22:31 +0930

    add RWMutex support for connection to libvirt server

--- libvirt/conn.go
+++ libvirt/conn.go
@@ -1,35 +1,46 @@
 package libvirt
 
 import (
-    "gopkg.in/alexzorin/libvirt-go.v2"
+	"gopkg.in/alexzorin/libvirt-go.v2"
+	"sync"
 )
 
+type GlobalConn struct {
+	conn_map map[string]*libvirt.VirConnection
+	lock     sync.RWMutex
+}
+
 var (
-    global_conn map[string]*libvirt.VirConnection
+	global_conn *GlobalConn
 )
 
 func init() {
-    global_conn = make(map[string]*libvirt.VirConnection, 0)
+	global_conn = new(GlobalConn)
+	global_conn.conn_map = make(map[string]*libvirt.VirConnection, 0)
 }
 
 func GetConn(host, port string) (*libvirt.VirConnection, error) {
-    var conn *libvirt.VirConnection
-    var err error
-    var ok bool
+	var conn *libvirt.VirConnection
+	var err error
+	var ok bool
 
-    if conn, ok = global_conn[host]; ok {
-        if ok, err = conn.IsAlive(); ok {
-            return conn, nil
-        }
-    }
+	global_conn.lock.RLock()
+	if conn, ok = global_conn.conn_map[host]; ok {
+		if ok, err = conn.IsAlive(); ok {
+			global_conn.lock.RUnlock()
+			return conn, nil
+		}
+	}
+	global_conn.lock.RUnlock()
 
-    new_conn, err := libvirt.NewVirConnection("qemu+tcp://"+host+":"+port+"/system")
-    if err != nil {
-        return &new_conn, err
-    }
+	new_conn, err := libvirt.NewVirConnection("qemu+tcp://" + host + ":" + port + "/system")
+	if err != nil {
+		return &new_conn, err
+	}
 
-    global_conn[host] = &new_conn
+	global_conn.lock.Lock()
+	global_conn.conn_map[host] = &new_conn
+	global_conn.lock.Unlock()
 
-    return &new_conn, nil
+	return &new_conn, nil
 }
-
